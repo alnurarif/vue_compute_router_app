c67e98fc726379867b3c1f06874aa95bb5d03e55
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2023-10-14 18:40:04 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref, reactive} from 'vue'
+import WatchComponent from "./components/WatchComponent.vue"
+import ComputeComponent from "./components/ComputeComponent.vue"
+
+const selectedComponent = ref('computed')
+</script>
+<template>
+	<div class="flex justify-center">
+		<div class=" w-full md:w-400 bg-blue-300 p-4">
+			<div class="flex justify-center">
+				<!-- <div class="text-center bg-blue-500 px-4 py-2 rounded-l-lg border-r border-white hover:bg-blue-700" :class="[$route.name == 'todo' ? 'bg-blue-700' : '']">Todo App</div> -->
+				<div @click="selectedComponent = 'computed'" class="w-52 cursor-pointer text-center bg-blue-500 px-4 py-2 rounded-l-lg border-r border-white hover:bg-blue-700" :class="[selectedComponent == 'computed' ? 'bg-blue-700' : '']">Computed</div>
+
+				<!-- <div class="text-center bg-blue-500 px-4 py-2 rounded-r-lg hover:bg-blue-700" :class="[$route.name != 'todo' ? 'bg-blue-700' : '']">Auth App</div> -->
+				<div @click="selectedComponent = 'watch'" class="w-52 cursor-pointer text-center bg-blue-500 px-4 py-2 rounded-r-lg hover:bg-blue-700" :class="[selectedComponent == 'watch' ? 'bg-blue-700' : '']">Watch</div>
+			</div>
+		</div>
+	</div>
+	
+	<WatchComponent v-if="selectedComponent == 'watch'"/>
+	<ComputeComponent v-if="selectedComponent == 'computed'"/>
+</template>
---
+++ src/components/ComputeComponent.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { computed, ref, watch } from 'vue';
+
+
+const user = ref({
+	name: "Al Nur Sarwer Arif",
+	email: "alnurarif@yahoo.com",
+	age: 33,
+	img: "https://images.unsplash.com/photo-1526800544336-d04f0cbfd700?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+	birthYear: null,
+	eligibleToVote: false,
+})
+const birthYear = computed(() => {
+	const currentYear = new Date().getFullYear();
+	return currentYear - user.value.age;
+})
+const canVote = computed(() => {
+	return user.value.age >= 18 ? "Eligible for voting" : "Not eligible for voting";
+})
+
+</script>
+
+<template>
+<div class="max-w-md mx-auto mt-8 p-4">
+    <h1 class="text-2xl font-semibold mb-4">User Details (Computed)</h1>
+    <div class="flex justify-center">
+        <img :src="user.img" class=" w-1/4 pr-5 pt-5 pb-5">
+        <div class="w-3/4 p-5">
+            <div class="mb-4">
+                <label for="birthYear" class="block text-gray-600 text-sm">Birth Year: {{ birthYear }}</label>
+            </div>
+            <div>
+                <label for="votingStatus" class="block text-gray-600  text-sm">Voting Status: {{ canVote }}</label>
+            </div>
+        </div>
+    </div>
+    <div class="mb-4">
+        <label for="name" class="block text-gray-600">Name:</label>
+        <input v-model="user.name" id="name" class="w-full border border-gray-300 rounded p-2" />
+    </div>
+    <div class="mb-4">
+        <label for="email" class="block text-gray-600">Email:</label>
+        <input v-model="user.email" id="email" type="email" class="w-full border border-gray-300 rounded p-2" />
+    </div>
+    <div class="mb-4">
+        <label for="age" class="block text-gray-600">Age:</label>
+        <input v-model.number="user.age" type="number" id="age" class="w-full border border-gray-300 rounded p-2" />
+    </div>
+    <div class="mb-4">
+        <label for="img" class="block text-gray-600">Profile Img:</label>
+        <input v-model="user.img" id="age" class="w-full border border-gray-300 rounded p-2" />
+    </div>
+
+</div>
+</template>
